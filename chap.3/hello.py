e2eb0b2469e73e124138ec6a1d0462b7105d9da2
김구태 <gootae0220@naver.com>
2024-08-27 21:15:12 +0900

    chap 3 end

--- chap.3/hello.py
+++ chap.3/hello.py
@@ -1,30 +1,45 @@
-from fastapi import FastAPI, Body, Response
+from fastapi import FastAPI, Header, Body, Response
 
 app = FastAPI()
-@app.get("/hi")
-def greet():
-    return "Hello? World?"
+
+# @app.get("/hi")
+# def greet():
+#     return "Hello? World?"
 
 # @app.get("/hi/{who}")
 # def greet(who):
 #     return f"Hello? {who}?"
 
-# @app.get("/hi")
-# def greet(who):
-#     return f"Hello? {who}?"
+@app.get("/hi")
+def greet(who):
+    return f"Hello? {who}?"
 
 @app.post("/hi")
 def greet(who: str = Body(embed = True)):
     return f"Hello? {who}?"
 
+# GET : localhost:8000/
+
+# Key : who, Value : Mom
+
+# p64
+@app.get("/agent")
+def get_agent(user_agent: str = Header()):
+    return user_agent
+
+# p66
 @app.get("/happy")
 def happy(status_code = 200):
     return ":)"
+
+# localhost:8000/header/marco/polo
 
 @app.get("/header/{name}/{value}")
 def header(name: str, value: str, response : Response):
     response.headers[name] = value
     return "normal body"
+
+
 
 if __name__ == "__main__":
     import uvicorn
