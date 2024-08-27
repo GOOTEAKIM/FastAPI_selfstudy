dfbb56f24b5e7cc8a9df38714d238ae9966f3d56
김구태 <gootae0220@naver.com>
2024-08-28 02:30:58 +0900

    chap 8 end

---
+++ chap.8/src/fake/creature.py
@@ -0,0 +1,57 @@
+from model.creature import Creature
+
+# 가짜 데이터 . 10장에서 실제 데이터베이스와 SQL 로 바꾼다
+
+from typing import Union
+
+_creatures = [
+    Creature(name='Yeti',
+             aka='Abominable Snowman',
+             country='CN',
+             area='Himalayas',
+             description='Hirsute Himalayan'),
+
+    Creature(name='Bigfoot',
+            description='Hirsute Himalayan',
+            country='US',
+            area='*',
+            aka="Sasquatch"
+            ),
+]
+
+def get_all() -> list[Creature]:
+    # 생명체 목록을 반환
+    return _creatures
+
+def get_one(name:str) -> Union[Creature, None]:
+    # 검색한 생명체 반환
+
+    for _creature in _creatures:
+        if _creature.name == name:
+            return _creature
+    return None
+
+# 다음 함수는 현재 올바로 동작하지 않는다
+# 실제로는 _explorers 목록을 수정하지 않지만,
+# 마치 작동하는 것처럼 동작한다.
+
+def create(creature: Creature) -> Creature:
+    # 생명체 추가
+    return creature
+
+def modify(name:str, creature: Creature) -> Creature:
+    # 생명체 정보 일부를 수정
+    return creature
+
+def replace(name:str, creature: Creature) -> Creature:
+    # 생명체를 완전히 교체
+    return creature
+
+def delete(name:str) -> bool:
+    # 생명체를 삭제, 만약에 대상이 없다면 False 반환
+
+    for _creature in _creatures:
+        if _creature.name == name:
+            return True
+    
+    return False
---
+++ chap.8/src/web/explorer.py
@@ -0,0 +1,37 @@
+from fastapi import APIRouter
+
+from model.explorer import Explorer
+import fake.explorer as service
+
+# 파이썬 3.9에서는 | 연산자 못쓴다.
+# 3.10 부터 가능하다.
+
+from typing import Union
+
+router = APIRouter(prefix='/explorer')
+
+@router.get('/')
+def get_all() -> list[Explorer]:
+    return service.get_all()
+
+@router.get('/{name}')
+def get_one(name) -> Union[Explorer, None]:
+    return service.get_one(name)
+
+# 나머지 엔드 포인트. 현재는 아무 일도 하지 않는다.
+
+@router.post('/')
+def create(explorer:Explorer) -> Explorer:
+    return service.create(explorer)
+
+@router.patch('/{name}')
+def modify(name, explorer:Explorer) -> Explorer:
+    return service.modify(name,explorer)
+
+@router.put('/{name}')
+def replace(name, explorer:Explorer) -> Explorer:
+    return service.replace(name, explorer)
+
+@router.delete('/{name}')
+def delete(name:str):
+    return None
