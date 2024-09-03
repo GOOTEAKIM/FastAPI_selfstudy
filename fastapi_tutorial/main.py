466da894ac2342f626518fd77d437a99835cd0b8
김구태 <gootae0220@naver.com>
2024-09-04 00:23:57 +0900

    ~ Query Parameters end

--- fastapi_tutorial/main.py
+++ fastapi_tutorial/main.py
@@ -8,13 +8,15 @@ class ModelName(str, Enum):
     resnet = 'resnet'
     lenet = 'lenet'
 
+# 경로 매개변수
+
 @app.get('/')
 async def root():
     return {'message': 'Hello World'}
 
-@app.get("/items/{item_id}")
-async def read_item(item_id : int):
-    return {"item_id":item_id}
+# @app.get("/items/{item_id}")
+# async def read_item(item_id : int):
+#     return {"item_id":item_id}
 
 @app.get('/users')
 async def read_users():
@@ -42,3 +44,47 @@ async def get_model(model_name: ModelName):
 async def read_file(file_path: str):
     return {'file_path': file_path}
 
+## 쿼리 매개변수
+
+fake_items_db = [{'item_name': 'Foo'}, {'item_name': 'Bar'}, {'item_name': 'oo'}]
+
+@app.get('/items/')
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip + limit]
+
+from typing import Union
+
+# 선택 매개변수
+
+'''
+@app.get('/items/{item_id}')
+async def read_item(item_id: str, q: Union[str, None] = None, short : bool = False):
+    
+    item = {'item_id': item_id}
+
+    if q:
+        return {'item_id': item_id, 'q': q}
+    
+    if not short:
+        item.update(
+            {'description': 'This is an amazing item that has a long description'}
+        )
+
+    return item
+'''
+
+# 다중 경로 및 쿼리 매개변수, 필수 쿼리 매개변수
+
+'''
+@app.get('/items/{item_id}')
+async def read_user_item(item_id: str, needy:str):
+    item = {'item_id': item_id, 'needy': needy}
+    return item
+'''
+
+@app.get('/items/{item_id}')
+async def read_user_item(item_id:str, needy:str, skip:int=0, limit: Union[int,None] = None):
+    item = {'item_id': item_id, 'needy': needy, 'skip': skip, 'limit': limit}
+    return item
+
+# http://127.0.0.1:8000/items/fooo?needy=hello&limit=3
