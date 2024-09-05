ff0aef012904c844ca8d63be3b1ee3309bcabda5
김구태 <gootae0220@naver.com>
2024-09-05 09:46:34 +0900

    ~ response model return type end

--- fastapi_tutorial/Response_model_return_type.py
+++ fastapi_tutorial/Response_model_return_type.py
@@ -1,6 +1,7 @@
-from typing import Union, Any
+from typing import Union, Any, List
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
+from fastapi.responses import JSONResponse, RedirectResponse
 from pydantic import BaseModel, EmailStr
 
 app = FastAPI()
@@ -9,14 +10,31 @@ class Item(BaseModel):
     name:str
     description: Union[str,None] = None
     price: float
-    tax: Union[float,None] = None
-    tags: list[str] = []
+    tax: float = 10.5
 
-class UserIn(BaseModel):
+items = {
+    "foo": {"name": "Foo", "price": 50.2},
+    "bar": {"name": "Bar", "description": "The Bar fighters", "price": 62, "tax": 20.2},
+    "baz": {
+        "name": "Baz",
+        "description": "There goes my baz",
+        "price": 50.2,
+        "tax": 10.5,
+    },
+}
+
+class BaseUser(BaseModel):
     username:str
-    password: str
+    email:EmailStr
+    full_name: Union[str,None]=None
+
+class UserOut(BaseModel):
+    username:str
     email:EmailStr
     full_name: Union[str, None]=None
+
+class UserIn(BaseModel):
+    password: str
 
 @app.post('/items/', response_model=Item)
 async def create_item(item: Item) -> Any:
@@ -29,9 +47,9 @@ async def read_items() -> Any:
         Item(name='Plumbus', price=32.0),
     ]
 
-@app.post("/user/")
-async def create_user(user: UserIn) -> UserIn:
-    return user
+# @app.post("/user/")
+# async def create_user(user: UserIn) -> UserIn:
+#     return user
 
 '''json
 {
@@ -41,4 +59,39 @@ async def create_user(user: UserIn) -> UserIn:
 }
 '''
 
-# 동일한 입력 데이터를 반환합니다.
+# @app.post('/user/', response_model=UserOut)
+# async def create_user(user:UserIn) -> Any:
+#     return user
+
+@app.post('/user/')
+async def create_user(user:UserIn) ->BaseModel:
+    return user
+
+# @app.get('/portal')
+# async def get_portal(teleport: bool = False) -> Response:
+#     if teleport:
+#         return RedirectResponse(url='https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+    
+#     return JSONResponse(content={"message": "Here's your interdimensional portal."})
+
+@app.get("/portal", response_model=None)  # response_model을 None으로 설정
+async def get_portal(teleport: bool = False) -> Union[Response, dict]:
+    if teleport:
+        return RedirectResponse(url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    return {"message": "Here's your interdimensional portal."}
+
+@app.get('/teleport')
+async def get_teleport() -> RedirectResponse:
+    return RedirectResponse(url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+
+@app.get('/items/{item_id}', response_model=Item, response_model_exclude_unset=True)
+async def read_item(item_id: str):
+    return items[item_id]
+
+@app.get('/items/{item_id}/name', response_model=Item, response_model_include=['name', 'description'],)
+async def read_item_name(item_id:str):
+    return items[item_id]
+
+@app.get('/items/{item_id}/public', response_model=Item, response_model_exclude={"tax"})
+async def read_item_public_data(item_id:str):
+    return items[item_id]
